b1a003640e7c7776facd0f6c59fe906db032d2db
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-06-19 19:11:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const fs = require('fs');
+
+const globalObject = {
+  globals: {
+    siteTitle: 'Lognetics - A Perfect Blend of Creativity',
+    description:
+      'Futuristic Domain of creativity, a team of creative and innovative experts, we deliver the most advanced tools and solutions. We create Branded identities for companies, Virtual and Augmented Reality, alongside Holographic Computation in every technology on all platforms. The future begins today!',
+    image: 'img/logo.png',
+    date: new Date().getFullYear(),
+  },
+};
+
+const data = JSON.stringify(globalObject);
+
+fs.writeFileSync('_harp.json', data);
